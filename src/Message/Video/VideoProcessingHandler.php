1a6a674ff81fc4227ee4dc4575995cdcd51dae96
Tony <tonydeveper@gmail.com>
2024-06-25 22:25:31 +0300

    If video processing dont delete input video

--- src/Message/Video/VideoProcessingHandler.php
+++ src/Message/Video/VideoProcessingHandler.php
@@ -6,6 +6,8 @@ use App\Entity\Video;
 use App\Service\VideoService;
 use App\ValueObject\FileSize;
 use Doctrine\ORM\EntityManagerInterface;
+use LogicException;
+use RuntimeException;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Workflow\Registry;
@@ -30,16 +32,15 @@ readonly class VideoProcessingHandler
         ;
 
         if (!$video) {
-            throw new \RuntimeException('Video not found');
+            throw new RuntimeException('Video not found');
         }
 
         $workflow = $this->workflowRegistry->get($video, 'video_processing');
 
         if ($workflow->can($video, 'start_processing')) {
             $workflow->apply($video, 'start_processing');
-        }
-        else {
-            throw new \LogicException('Video cannot be processed.');
+        } else {
+            throw new LogicException('Video cannot be processed.');
         }
         // Сохранение сущности Video после изменения состояния
         $this->entityManager->flush();
@@ -47,17 +48,18 @@ readonly class VideoProcessingHandler
         try {
             $videoOutput = $this->videoService->processVideo($message);
 
-            if ($workflow->can($video, 'complete')) {
-                $workflow->apply($video, 'complete');
-            }
-            else {
-                throw new \LogicException('Video cannot be completed.');
-            }
-
             $video->setSize($videoOutput->fileSize->convertTo(FileSize::UNIT_KILOBYTE))
                   ->setImagePreview($videoOutput->posterPath)
                   ->setPath($videoOutput->videoPath)
             ;
+
+            $workflow->can($video, 'complete')
+                ? $workflow->apply($video, 'complete')
+                : throw new LogicException('Video cannot be completed.');
+
+            # Загруженное видео удаляется только если processVideo произошел успешно
+            $directoryPath = dirname($message->videoInputPath);
+            $this->filesystem->remove($directoryPath);
         } catch (\Exception $e) {
             // Применяем переход 'fail' в случае ошибки
             if ($workflow->can($video, 'fail')) {
@@ -68,8 +70,5 @@ readonly class VideoProcessingHandler
 
         $this->entityManager->persist($video);
         $this->entityManager->flush();
-
-        $directoryPath = dirname($message->videoInputPath);
-        $this->filesystem->remove($directoryPath);
     }
 }
